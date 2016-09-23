a5fb56e31383b3d5cb53314533bbe801fbcda09a
Fangdun Cai (Fundon) <cfddream@gmail.com>
2016-09-24 00:59:32 +0800

    close #1

--- src/index.js
+++ src/index.js
@@ -1,56 +1,56 @@
 import nprogress from 'nprogress'
 
-export default class NProgress {
+function install (Vue) {
+  if (this.installed) return
+  this.installed = true
 
-  static install (Vue) {
-    if (this.installed) return
-    this.installed = true
+  Object.defineProperty(Vue.prototype, '$nprogress', {
+    get: function get () { return this.$root._nprogress }
+  })
 
-    Object.defineProperty(Vue.prototype, '$nprogress', {
-      get () { return this.$root._nprogress }
-    })
+  Vue.mixin({
+    beforeCreate: function beforeEach () {
+      const np = this.$options.nprogress
+      if (np) {
+        this._nprogress = np
+        np.init(this)
 
-    Vue.mixin({
-      beforeCreate () {
-        const np = this.$options.nprogress
-        if (np) {
-          this._nprogress = np
-          np.init(this)
-
-          if (Vue.http) {
-            Vue.http.interceptors.push((request, next) => {
-              np.inc(0.2)
-              next(response => {
-                np.done()
-                return response
-              })
-            })
-          }
-
-          const router = this.$options.router
-          if (router) {
-            router.beforeEach((route, redirect, next) => {
-              np.start()
-              next()
-            })
-            router.afterEach(() => {
+        if (Vue.http) {
+          Vue.http.interceptors.push(function (request, next) {
+            np.inc(0.2)
+            next(function (response) {
               np.done()
+              return response
             })
-          }
+          })
+        }
+
+        const router = this.$options.router
+        if (router) {
+          router.beforeEach(function (route, redirect, next) {
+            np.start()
+            next()
+          })
+          router.afterEach(function () {
+            np.done()
+          })
         }
       }
-    })
-  }
-
-  constructor (options = {}) {
-    this.app = null
-    this.configure(options)
-  }
-
-  init (app) {
-    this.app = app
-  }
-
+    }
+  })
 }
 
+function NProgress (options) {
+  this.app = null
+  this.configure(options || {})
+}
+
+NProgress.prototype.init = function (app) {
+  this.app = app
+}
+
+NProgress.install = install
+
 Object.setPrototypeOf(NProgress.prototype, nprogress)
+
+export default NProgress
